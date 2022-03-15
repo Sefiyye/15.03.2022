d32be0b12437ef4855950b11d0800842a2e5e155
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-15 11:39:36 +0400

    15mart Task

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String());
+        }
+        public static string String()
+        {
+            string sentence = "bU tasKI metHodla yAzmAliSiz";
+            sentence = sentence.ToLower();
+            sentence = sentence.Replace("b", "B");
+            sentence = sentence.Replace("ta", "Ta");
+            sentence = sentence.Replace("me", "Me");
+            sentence = sentence.Replace("y", "Y");
+
+            return sentence;
+        }
+    }
+}
